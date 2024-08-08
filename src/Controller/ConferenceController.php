ed18cd7ad2b92dc3a6f1641938e847a3e63e1219
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-08-08 16:30:48 +0300

    Get conferences data to view

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/c-soon', name: 'c_soon')]
+    public function cSoon(): Response
     {
         return new Response(<<<EOF
 <html>
@@ -19,5 +22,22 @@ class ConferenceController extends AbstractController
 </html>
 EOF
         );
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments'   => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC'])
+        ]);
     }
 }
