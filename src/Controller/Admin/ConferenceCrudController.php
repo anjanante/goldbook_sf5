ee31c643afd51d12bdb8a0a0aadd2add542557e1
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-08-08 11:41:15 +0300

    Manage conference list filter

--- src/Controller/Admin/ConferenceCrudController.php
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -13,6 +15,23 @@ class ConferenceCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Conference::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference')
+            ->setEntityLabelInPlural('Conferences')
+            ->setSearchFields(['city', 'year'])
+            ->setDefaultSort(['year' => 'DESC'])
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('isInternational')
+        ;
     }
 
     /*
