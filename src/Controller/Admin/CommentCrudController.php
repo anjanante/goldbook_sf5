5414a588bbd6856c2ce3b15d43a73d57a6aaed10
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-08-08 11:02:51 +0300

    Manage filed to edit for comment

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -15,14 +17,17 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('author'),
+            TextEditorField::new('text'),
+            EmailField::new('email'),
+            TextField::new('conference'),
+            AssociationField::new('conference'),
+            DateTimeField::new('createdAt'),
         ];
     }
-    */
+    
 }
